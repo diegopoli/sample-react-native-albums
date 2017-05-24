6a01d9995de21727771d7c9e2d2363db3ab3587c
Diego Poli <diego.apr@gmail.com>
2017-05-24 14:33:00 -0700

    Firebase initialization

--- auth/src/App.js
+++ auth/src/App.js
@@ -1,8 +1,23 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
 
 class App extends Component {
+  // This is a lifecycle method invoked immediately before mounting occurs,
+  // during server rendering.
+  componentWillMount() {
+    // Firebase initialization
+    firebase.initializeApp({
+      apiKey: 'AIzaSyC9Zz7EKY2jQMDnryyoVqyauytCBSc7MNk',
+      authDomain: 'react-native-albums-auth.firebaseapp.com',
+      databaseURL: 'https://react-native-albums-auth.firebaseio.com',
+      projectId: 'react-native-albums-auth',
+      storageBucket: 'react-native-albums-auth.appspot.com',
+      messagingSenderId: '332098616101'
+    });
+  }
+
   render() {
     return (
       <View>
