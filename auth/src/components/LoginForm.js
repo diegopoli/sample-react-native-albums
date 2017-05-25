df90d96fbca564f9a7d94a9afe9341c8c2e79f90
Diego Poli <diego.apr@gmail.com>
2017-05-25 14:39:56 -0700

    Working on input text component.

--- auth/src/components/LoginForm.js
+++ auth/src/components/LoginForm.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
-import { Button, Card, CardSection } from './common';
+import { Button, Card, CardSection, Input } from './common';
 
 class LoginForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { text: '' };
+  }
+
   render() {
     return (
       <Card>
+        <CardSection>
+          <Input
+            label="Email"
+            onChangeText={text => this.setState({ text })}
+            value={this.state.text}
+          />
+        </CardSection>
+
         <CardSection />
-        <CardSection />
+
         <CardSection>
           <Button>
             Log In
