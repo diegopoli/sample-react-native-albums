d44ebdc58740cea589a90642be700c6ea0a1d67f
Diego Poli <diego.apr@gmail.com>
2017-05-27 00:09:37 -0700

    Added sign out button.

--- auth/src/App.js
+++ auth/src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { View, Text } from 'react-native';
 import firebase from 'firebase';
-import { Header } from './components/common';
+import { Header, Button, Spinner, Card, CardSection } from './components/common';
 import LoginForm from './components/LoginForm';
 
 class App extends Component {
+  state = { loggedIn: null, username: '' };
+
   // This is a lifecycle method invoked immediately before mounting occurs,
   // during server rendering.
   componentWillMount() {
@@ -17,16 +19,62 @@ class App extends Component {
       storageBucket: 'react-native-albums-auth.appspot.com',
       messagingSenderId: '332098616101'
     });
+
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ loggedIn: true, username: user.email });
+      } else {
+        this.setState({ loggedIn: false, username: '' });
+      }
+    });
+  }
+
+  renderContent() {
+    const { welcomeMessageTextStyle, usernameTextStyle } = styles;
+
+    switch (this.state.loggedIn) {
+      case true:
+        return (
+          <Card>
+            <CardSection>
+              <Text style={welcomeMessageTextStyle}>
+                Welcome, <Text style={usernameTextStyle}>{this.state.username}</Text>!
+              </Text>
+            </CardSection>
+
+            <CardSection>
+              <Button onPress={() => firebase.auth().signOut()}>
+                Log Out
+              </Button>
+            </CardSection>
+          </Card>
+        );
+      case false:
+        return <LoginForm />;
+      default:
+        return <Spinner size="large" />;
+    }
   }
 
   render() {
     return (
       <View>
         <Header headerText="Authentication" />
-        <LoginForm />
+        {this.renderContent()}
       </View>
     );
   }
 }
+
+const styles = {
+  welcomeMessageTextStyle: {
+    fontSize: 16,
+    color: '#000',
+    marginLeft: 5
+  },
+  usernameTextStyle: {
+    fontWeight: 'bold'
+  }
+};
 
 export default App;
