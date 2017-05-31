24016abe37706899cd61dadcf49e83d4da0ea4f4
Diego Poli <diego.apr@gmail.com>
2017-05-31 16:45:23 -0700

    Initial commit.

---
+++ techstack/src/app.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { View } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from './reducers';
+import { Header } from './components/common';
+import LibraryList from './components/LibraryList';
+
+const App = (() =>
+  <Provider store={createStore(reducers)}>
+    {
+      // Providers support one single child component. However, this child can
+      // have as many subcomponents as needed.
+    }
+    <View>
+      <Header headerText="Tech Stack" />
+      <LibraryList />
+    </View>
+  </Provider>
+);
+
+export default App;
