e8e7fc3dee45f10b78fa200ae79e57968e24f167
Diego Poli <diego.apr@gmail.com>
2017-06-12 14:12:39 -0700

    Added employee form.

--- manager/src/actions/types.js
+++ manager/src/actions/types.js
@@ -1,5 +1,9 @@
+// Login Form
 export const EMAIL_CHANGED = 'email_changed';
 export const PASSWORD_CHANGED = 'password_changed';
 export const LOGIN_USER_SUCCESS = 'user_login_success';
 export const LOGIN_USER_FAIL = 'user_login_fail';
 export const LOGIN_USER = 'user_login';
+
+// Employee Form
+export const EMPLOYEE_UPDATE = 'employee_update';
