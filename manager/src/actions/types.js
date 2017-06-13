d531683d310bc7b9aee12388a4ce85ee22ae23d8
Diego Poli <diego.apr@gmail.com>
2017-06-13 16:29:46 -0700

    Implemented method for fetching data from Firebase.

--- manager/src/actions/types.js
+++ manager/src/actions/types.js
@@ -8,3 +8,4 @@ export const LOGIN_USER = 'user_login';
 // Employee Form
 export const EMPLOYEE_UPDATE = 'employee_update';
 export const EMPLOYEE_CREATE = 'employee_create';
+export const EMPLOYEES_FETCH_SUCCESS = 'employees_fetch_success';
